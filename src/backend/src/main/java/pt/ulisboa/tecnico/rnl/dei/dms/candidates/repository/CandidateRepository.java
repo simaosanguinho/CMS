907232086c2e4b9bbce509e26cfa6af25db89aa2
Simão Sanguinho <simao.sanguinho@tecnico.ulisboa.pt>
2024-07-30 17:55:26 +0100

    feat: add repository query - find candidate by ist id

--- src/backend/src/main/java/pt/ulisboa/tecnico/rnl/dei/dms/candidates/repository/CandidateRepository.java
+++ src/backend/src/main/java/pt/ulisboa/tecnico/rnl/dei/dms/candidates/repository/CandidateRepository.java
@@ -3,8 +3,14 @@ package pt.ulisboa.tecnico.rnl.dei.dms.candidates.repository;
 import org.springframework.stereotype.Repository;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 import pt.ulisboa.tecnico.rnl.dei.dms.candidates.domain.Candidate;
 
 @Repository
 @Transactional
-public interface CandidateRepository extends JpaRepository<Candidate, Long> {}
+public interface CandidateRepository extends JpaRepository<Candidate, Long> {
+
+    @Query("SELECT c FROM Candidate c WHERE c.istID = :istID")
+    List<Candidate> findByIstID(String istID);
+}
