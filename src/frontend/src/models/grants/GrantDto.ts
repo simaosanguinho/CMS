2936c5061917b6853a42dbdfa4c6c6ec91d77b83
Simão Sanguinho <simao.sanguinho@tecnico.ulisboa.pt>
2024-08-01 12:37:28 +0100

    fix: convert dates on grant dto frontend to strings

--- src/frontend/src/models/grants/GrantDto.ts
+++ src/frontend/src/models/grants/GrantDto.ts
@@ -1,13 +1,20 @@
 import CandidateDto from '@/models/candidates/CandidateDto'
+import { ISOtoString } from '@/services/ConvertDateService'
 
 export default class GrantDto {
     id?: string
-    startDate?: string
-    endDate?: string
+    startDate!: string
+    endDate!: string
     monthlyIncome?: number
     candidates?: CandidateDto[]
 
     constructor(jsonObj: Partial<GrantDto>) {
-        Object.assign(this, jsonObj)
+        if (jsonObj) {
+            this.id = jsonObj.id
+            this.startDate = ISOtoString(jsonObj.startDate ?? null) 
+            this.endDate = ISOtoString(jsonObj.endDate ?? null) 
+            this.monthlyIncome = jsonObj.monthlyIncome
+            this.candidates = jsonObj.candidates ? jsonObj.candidates.map(candidate => new CandidateDto(candidate)) : []
+        }
     }
 }
