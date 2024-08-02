da7dba6c836ad7d7f7d38d0afed9445115d92d2d
Simão Sanguinho <simao.sanguinho@tecnico.ulisboa.pt>
2024-08-02 17:20:32 +0100

    feat: add enrollments controller backend

---
+++ src/backend/src/main/java/pt/ulisboa/tecnico/rnl/dei/dms/enrollments/EnrollmentController.java
@@ -0,0 +1,37 @@
+package pt.ulisboa.tecnico.rnl.dei.dms.enrollments;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import pt.ulisboa.tecnico.rnl.dei.dms.enrollments.dto.EnrollmentDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+
+@RestController()
+@RequestMapping
+public class EnrollmentController {
+    
+    @Autowired
+    EnrollmentService enrollmentService;
+
+    @PostMapping("/grants/{grantId}/enrollments/{candidateId}")
+    public EnrollmentDto createEnrollment(@PathVariable Long candidateId,@PathVariable Long grantId, @RequestBody EnrollmentDto enrollmentDto) {
+        return enrollmentService.createEnrollment(candidateId, grantId, enrollmentDto);
+    }
+
+    @GetMapping("/grants/{grantId}/enrollments")
+    public List<EnrollmentDto> getEnrollmentsByGrantId(@PathVariable Long grantId) {
+        return enrollmentService.getEnrollmentsByGrantId(grantId);
+    }
+
+    @GetMapping("/candidates/enrollments/{candidateId}")
+    public List<EnrollmentDto> getEnrollmentsByCandidateId(@PathVariable Long candidateId) {
+        return enrollmentService.getEnrollmentsByCandidateId(candidateId);
+    }
+}
