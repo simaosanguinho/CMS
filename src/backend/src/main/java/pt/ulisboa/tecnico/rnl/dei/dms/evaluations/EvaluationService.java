0076cfb11ca5f4315cfca9c05c7bef8e928936ba
Simão Sanguinho <simao.sanguinho@tecnico.ulisboa.pt>
2024-08-03 19:42:13 +0100

    feat: add evaluation service on backend

---
+++ src/backend/src/main/java/pt/ulisboa/tecnico/rnl/dei/dms/evaluations/EvaluationService.java
@@ -0,0 +1,27 @@
+package pt.ulisboa.tecnico.rnl.dei.dms.evaluations;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+
+import pt.ulisboa.tecnico.rnl.dei.dms.evaluations.domain.Evaluation;
+import pt.ulisboa.tecnico.rnl.dei.dms.evaluations.dto.EvaluationDto;
+import pt.ulisboa.tecnico.rnl.dei.dms.evaluations.repository.EvaluationRepository;
+
+@Service
+public class EvaluationService {
+    
+    @Autowired
+    private EvaluationRepository evaluationRepository;
+
+    @Transactional(isolation = Isolation.READ_COMMITTED)
+    public EvaluationDto createEvaluation(EvaluationDto evaluationDto) {
+        Evaluation evaluation = new Evaluation(evaluationDto);
+        evaluationRepository.save(evaluation);
+        return new EvaluationDto(evaluation);
+    }
+
+
+}
