fb476826fbf8f9b3e687885a95a3799b9c7e3088
Simão Sanguinho <simao.sanguinho@tecnico.ulisboa.pt>
2024-08-03 19:42:03 +0100

    feat: add evaluation repository on backend

---
+++ src/backend/src/main/java/pt/ulisboa/tecnico/rnl/dei/dms/evaluations/repository/EvaluationRepository.java
@@ -0,0 +1,23 @@
+package pt.ulisboa.tecnico.rnl.dei.dms.evaluations.repository;
+
+import org.springframework.stereotype.Repository;
+
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
+import java.util.Optional;
+import pt.ulisboa.tecnico.rnl.dei.dms.evaluations.domain.Evaluation;
+
+
+@Repository
+@Transactional
+public interface EvaluationRepository extends JpaRepository<Evaluation, Long> {
+
+    @Query("SELECT e FROM Evaluation e WHERE e.id = :id")
+    Optional<Evaluation> findById(Long id);
+
+    @Query("SELECT e FROM Evaluation e WHERE e.enrollment.id = :enrollmentId")
+    List<Evaluation> findByEnrollmentId(Long enrollmentId);
+
+}
