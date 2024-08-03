7985595ed7514d0b2df824a9cc24616cff7c927a
Simão Sanguinho <simao.sanguinho@tecnico.ulisboa.pt>
2024-08-03 19:41:24 +0100

    fix: change import for transactional annotation on enrollment repository

--- src/backend/src/main/java/pt/ulisboa/tecnico/rnl/dei/dms/enrollments/repository/EnrollmentRepository.java
+++ src/backend/src/main/java/pt/ulisboa/tecnico/rnl/dei/dms/enrollments/repository/EnrollmentRepository.java
@@ -1,7 +1,7 @@
 package pt.ulisboa.tecnico.rnl.dei.dms.enrollments.repository;
 
 import org.springframework.stereotype.Repository;
-import jakarta.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import java.util.List;
